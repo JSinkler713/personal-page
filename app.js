fe858e64b908546e9d0fd078e2a4e466aac28da6
James Sinkler <jamessinkler713@gmail.com>
2020-03-11 09:40:21 -0700

    check app.js is connected

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+'use strict'
+
+let hobbyHeader = document.querySelector('.hobbies-header')
+
+hobbyHeader.addEventListener('click', function sayHi() {
+  console.log('checkcheck header clicked');
+});
 
